90f522e09ba20c6163d66bd054b6d5616e4c20d5
Danila <crowo4ka@gmail.com>
2025-04-09 02:10:20 +0300

    init: gradle added

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "dev.donutquine"
+version = "0.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
