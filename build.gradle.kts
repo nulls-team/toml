c9bdda6c1f57ccb809a4d834b5f7b77c4d62fe88
Danila Schelkov <me@donutquine.dev>
2026-03-03 22:48:22 +0300

    chore(gradle): update gradle to 9.3.1 and add test runtime launcher
    https://github.com/gradle/gradle/issues/34512

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
@@ -30,6 +31,9 @@ tasks.test {
 java {
     withJavadocJar()
     withSourcesJar()
+    // toolchain {
+    //     languageVersion = JavaLanguageVersion.of(11)
+    // }
 }
 
 publishing {
